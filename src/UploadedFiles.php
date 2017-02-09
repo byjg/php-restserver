dec7f90ec6e1780d701739ca51bb3f3ed6da03a4
Joao Gilberto Magalhaes <joao@byjg.com.br>
2017-02-09 12:14:50 -0200

    Added UploadFiles class

---
+++ src/UploadedFiles.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * User: jg
+ * Date: 09/02/17
+ * Time: 12:01
+ */
+
+namespace ByJG\RestServer;
+
+
+class UploadedFiles
+{
+    public function count()
+    {
+        return count($_FILES);
+    }
+
+    public function getKeys()
+    {
+        return array_keys($_FILES);
+    }
+
+    public function isOk($key)
+    {
+        return $this->getFileByKey($key, 'error');
+    }
+
+    public function getUploadedFile($key)
+    {
+        return file_get_contents($this->getFileByKey($key, 'tmp_name'));
+    }
+
+    public function getFileName($key)
+    {
+        return $this->getFileByKey($key, 'name');
+    }
+
+    public function saveTo($key, $destinationPath, $newName = "")
+    {
+        if (empty($newName)) {
+            $newName = $this->getFileName($key);
+        }
+
+        move_uploaded_file($this->getFileByKey($key, 'tmp_name'), $destinationPath . '/' . $newName);
+    }
+
+    public function clearTemp($key)
+    {
+        unlink($this->getFileByKey($key, 'tmp_name'));
+    }
+
+    private function getFileByKey($key, $property)
+    {
+        if (!isset($_FILES[$key])) {
+            throw new \InvalidArgumentException("The upload '$key' does not exists");
+        }
+
+        return $_FILES[$key][$property];
+    }
+}
