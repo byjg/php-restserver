c777854b9a509899894cf97b87a27cf232d6d6f5
Joao Gilberto Magalhaes <joao@byjg.com.br>
2017-10-14 23:38:21 -0300

    Added new way to get a response bag.

---
+++ tests/ModelSample.php
@@ -0,0 +1,53 @@
+<?php
+
+class ModelSample
+{
+    protected $prop1;
+    protected $prop2;
+
+    /**
+     * ModelSample constructor.
+     *
+     * @param $prop1
+     * @param $prop2
+     */
+    public function __construct($prop1, $prop2)
+    {
+        $this->prop1 = $prop1;
+        $this->prop2 = $prop2;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProp1()
+    {
+        return $this->prop1;
+    }
+
+    /**
+     * @param mixed $prop1
+     */
+    public function setProp1($prop1)
+    {
+        $this->prop1 = $prop1;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProp2()
+    {
+        return $this->prop2;
+    }
+
+    /**
+     * @param mixed $prop2
+     */
+    public function setProp2($prop2)
+    {
+        $this->prop2 = $prop2;
+    }
+
+
+}
