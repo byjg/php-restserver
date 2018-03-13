864904bc0b03820ccc1b909b9efc37ce730d1a72
Joao Gilberto Magalhães <joao@byjg.com.br>
2018-03-13 14:31:09 -0300

    Added the class ErrorCustomStatusException

---
+++ src/Exception/ErrorCustomStatusException.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ByJG\RestServer\Exception;
+
+use Throwable;
+
+class ErrorCustomStatusException extends ClientShowException
+{
+    protected $status;
+    protected $description;
+
+    public function __construct($message = "", $code = 400, \Throwable $previous = null)
+    {
+        $this->status = $code;
+        $this->description = $message;
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function handleHeader()
+    {
+        $this->sendHeader($this->status, $this->description);
+    }
+}
