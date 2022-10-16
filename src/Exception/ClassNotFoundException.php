26cfc9e29a560ef128f12512fa7cf84e2391a47f
Joao Gilberto <joao@byjg.com.br>
2022-10-16 08:50:13 -0300

    Fix error code class not found

--- src/Exception/ClassNotFoundException.php
+++ src/Exception/ClassNotFoundException.php
@@ -2,7 +2,7 @@
 
 namespace ByJG\RestServer\Exception;
 
-class ClassNotFoundException extends Error404Exception
+class ClassNotFoundException extends Error501Exception
 {
     //put your code here
 }
