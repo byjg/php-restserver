6cc217971f9e30716018ca20c032bdbc294e18aa
Joao Gilberto Magalhaes <joao@byjg.com.br>
2023-02-15 22:41:23 -0600

    More CORS adjustments

--- src/Middleware/CorsMiddleware.php
+++ src/Middleware/CorsMiddleware.php
@@ -42,20 +42,18 @@ class CorsMiddleware implements BeforeMiddlewareInterface
         HttpRequest $request
     )
     {
-        // TODO: Still missing some headers
-        // https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
-        if (strtoupper($request->server('REQUEST_METHOD')) != 'OPTIONS' || empty($request->server('HTTP_ORIGIN'))) {
-            return MiddlewareResult::continue();
+        $corsStatus = $this->preFlight($response, $request);
+        if ($corsStatus != self::CORS_OK) {
+            if ($corsStatus == self::CORS_OPTIONS) {
+                $response->emptyResponse();
+                $response->getResponseBag()->setSerializationRule(ResponseBag::RAW);
+                return MiddlewareResult::stopProcessingOthers();
+            } elseif ($corsStatus == self::CORS_FAILED) {
+                throw new Error401Exception("CORS verification failed. Request Blocked.");
+            }
         }
 
-        $corsStatus = $this->preFlight($response, $request);
-        if ($corsStatus == self::CORS_OPTIONS) {
-            $response->emptyResponse();
-            $response->getResponseBag()->setSerializationRule(ResponseBag::RAW);
-            return MiddlewareResult::stopProcessingOthers();
-        } else {
-            throw new Error401Exception("CORS verification failed. Request Blocked.");
-        }
+        return MiddlewareResult::continue();
     }
 
     /**
@@ -67,19 +65,32 @@ class CorsMiddleware implements BeforeMiddlewareInterface
      */
     protected function preFlight(HttpResponse $response, HttpRequest $request)
     {
-        foreach ((array)$this->corsOrigins as $origin) {
-            if (preg_match("~^.*//$origin$~", $request->server('HTTP_ORIGIN'))) {
-                $response->setResponseCode(204, 'No Content');
-                $response->addHeader("Access-Control-Allow-Origin", $request->server('HTTP_ORIGIN'));
-                $response->addHeader('Access-Control-Allow-Credentials', 'true');
-                $response->addHeader('Access-Control-Max-Age', '86400');    // cache for 1 day
-                $response->addHeader("Access-Control-Allow-Methods", implode(",", array_merge(['OPTIONS'], $this->corsMethods)));
-                $response->addHeader("Access-Control-Allow-Headers", implode(",", $this->corsHeaders));
-                return self::CORS_OPTIONS;
+        // TODO: Still missing some headers
+        // https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
+        $corsStatus = self::CORS_OK;
+
+        if (!empty($request->server('HTTP_ORIGIN'))) {
+            $corsStatus = self::CORS_FAILED;
+
+            foreach ((array)$this->corsOrigins as $origin) {
+                if (preg_match("~^.*//$origin$~", $request->server('HTTP_ORIGIN'))) {
+                    $response->addHeader("Access-Control-Allow-Origin", $request->server('HTTP_ORIGIN'));
+                    $response->addHeader('Access-Control-Allow-Credentials', 'true');
+                    $response->addHeader('Access-Control-Max-Age', '86400');    // cache for 1 day
+
+                    // Access-Control headers are received during OPTIONS requests
+                    if ($request->server('REQUEST_METHOD') == 'OPTIONS') {
+                        $response->setResponseCode(204, 'No Content');
+                        $response->addHeader("Access-Control-Allow-Methods", implode(",", array_merge(['OPTIONS'], $this->corsMethods)));
+                        $response->addHeader("Access-Control-Allow-Headers", implode(",", $this->corsHeaders));
+                        return self::CORS_OPTIONS;
+                    }
+                    $corsStatus = self::CORS_OK;
+                    break;
+                }
             }
         }
-
-        return self::CORS_FAILED;
+        return $corsStatus;
     }
 
     public function withCorsOrigins($origins)
