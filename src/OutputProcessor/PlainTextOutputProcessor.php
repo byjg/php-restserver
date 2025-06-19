fcbf4d4a7e0395335decfc15ff48f4995b4b2964
Joao Gilberto Magalhaes <joao@byjg.com.br>
2025-06-18 21:01:27 -0500

    Merge master

--- src/OutputProcessor/PlainTextOutputProcessor.php
+++ src/OutputProcessor/PlainTextOutputProcessor.php
@@ -5,6 +5,7 @@ namespace ByJG\RestServer\OutputProcessor;
 use ByJG\RestServer\Whoops\PlainResponseErrorHandler;
 use ByJG\Serializer\Formatter\FormatterInterface;
 use ByJG\Serializer\Formatter\PlainTextFormatter;
+use Override;
 use Whoops\Handler\Handler;
 
 class PlainTextOutputProcessor extends BaseOutputProcessor
@@ -15,16 +16,19 @@ class PlainTextOutputProcessor extends BaseOutputProcessor
         $this->contentType = "text/plain";
     }
 
+    #[Override]
     public function getDetailedErrorHandler(): Handler
     {
         return new PlainResponseErrorHandler();
     }
 
+    #[Override]
     public function getErrorHandler(): Handler
     {
         return $this->getDetailedErrorHandler();
     }
 
+    #[Override]
     public function getFormatter(): FormatterInterface
     {
         return new PlainTextFormatter();
